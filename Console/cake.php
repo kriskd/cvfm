905ece74345f648d436b51a5034fdc3dc39259d5
Kris Dockter <kriskd@gmail.com>
2014-01-12 23:14:21 +0000

    Path to core

--- Console/cake.php
+++ Console/cake.php
@@ -19,7 +19,8 @@
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
 $ds = DIRECTORY_SEPARATOR;
-$dispatcher = 'Cake' . $ds . 'Console' . $ds . 'ShellDispatcher.php';
+$root = dirname(dirname(dirname(__FILE__)));
+$dispatcher = $root . $ds . 'cakephp' . $ds . 'lib' . $ds . 'Cake' . $ds . 'Console' . $ds . 'ShellDispatcher.php';
 
 if (function_exists('ini_set')) {
 	$root = dirname(dirname(dirname(__FILE__)));
