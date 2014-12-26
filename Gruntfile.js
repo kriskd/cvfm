eabdeb090c1858299cfdbe24334b10efdca755e9
Kris Dockter <kriskd@gmail.com>
2014-12-26 11:54:44 -0600

    General project update

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,4 @@
+var autoprefixer = require('autoprefixer-core');
 module.exports = function(grunt) {
 	'use strict';
 
@@ -55,23 +56,33 @@ module.exports = function(grunt) {
 				ext: '.min.css'
 			}
 		},
-                watch: {
-			jss: {
-				files: ['webroot/js/src/*.js'],
-				tasks: ['concat', 'uglify']
-			},
-			css: {
-				files: ['webroot/css/src/styles.less', 'webroot/css/src/admin.less'],
-				tasks: ['less', 'cssmin']
-			}
-                },
+        postcss: {
+          options: {
+            processors: [
+              autoprefixer({ browsers: ['last 2 version'] }).postcss
+              ]
+          },
+          dist: { src: 'webroot/css/build/*.css' }
+       },
+       watch: {
+         jss: {
+           files: ['webroot/js/src/*.js'],
+           tasks: ['concat', 'uglify']
+         },
+         css: {
+           files: ['webroot/css/src/styles.less', 'webroot/css/src/admin.less'],
+           tasks: ['less', 'cssmin', 'postcss']
+         }
+       }
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
-        grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-postcss');
+    grunt.loadNpmTasks('grunt-autoprefixer');
 
 	grunt.registerTask('default', ['concat:dist', 'uglify']);
 };
