68e83f81c97a09c1a88dce36ca94600783292be4
Kris Dockter <kriskd@gmail.com>
2014-12-26 19:25:41 -0600

    File cleanup

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,62 +5,62 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		concat: {
-			options: {
-				separator: ';',
-			},
-			dist: {
-				src: [
-					'webroot/js/src/jquery.galleriffic.js',
-					'webroot/js/src/modal.js',
-					'webroot/js/src/tooltip.js',
-					'webroot/js/src/js.js',
-				],
-				dest: 'webroot/js/build/scripts.js'
-			}
+          options: {
+            separator: ';',
+          },
+          dist: {
+            src: [
+              'webroot/js/src/jquery.galleriffic.js',
+              'webroot/js/src/modal.js',
+              'webroot/js/src/tooltip.js',
+              'webroot/js/src/js.js',
+            ],
+            dest: 'webroot/js/build/scripts.js'
+          }
 		},
 		uglify: {
-			options: {
-				mangle: false
-			},
-			my_target: {
-				files: {
-					'webroot/js/build/scripts.min.js': ['webroot/js/build/scripts.js']
-				}
-			}
+          options: {
+            mangle: false
+          },
+          my_target: {
+              files: {
+                'webroot/js/build/scripts.min.js': ['webroot/js/build/scripts.js']
+              }
+          }
 		},
 		less:	{
-			my_target: {
-				files: {
-					'webroot/css/build/styles.css': 'webroot/css/src/styles.less',
-				}
-			},
-			admin : {
-				files : {
-					'webroot/css/build/admin.css': 'webroot/css/src/admin.less'
-				}
-			}
+          my_target: {
+            files: {
+              'webroot/css/build/styles.css': 'webroot/css/src/styles.less',
+            }
+          },
+          admin : {
+            files : {
+              'webroot/css/build/admin.css': 'webroot/css/src/admin.less'
+            }
+          }
 		},
 		cssmin: {
-			my_target: {
-				expand: true,
-				cwd: 'webroot/css/build/',
-				src: ['styles.css', '!*.min.css'],
-				dest: 'webroot/css/build/',
-				ext: '.min.css'
-			},
-			admin : {
-				expand: true,
-				cwd: 'webroot/css/build/',
-				src: ['admin.css', '!*.min.css'],
-				dest: 'webroot/css/build/',
-				ext: '.min.css'
-			}
+          my_target: {
+            expand: true,
+            cwd: 'webroot/css/build/',
+            src: ['styles.css', '!*.min.css'],
+            dest: 'webroot/css/build/',
+            ext: '.min.css'
+          },
+          admin : {
+            expand: true,
+            cwd: 'webroot/css/build/',
+            src: ['admin.css', '!*.min.css'],
+            dest: 'webroot/css/build/',
+            ext: '.min.css'
+          }
 		},
         postcss: {
           options: {
             processors: [
               autoprefixer({ browsers: ['last 2 version'] }).postcss
-              ]
+            ]
           },
           dist: { src: 'webroot/css/build/*.css' }
        },
@@ -68,8 +68,8 @@ module.exports = function(grunt) {
          options: {
            srcPrefix: 'bower_components',
            destPrefix: 'webroot/css'
-       },
-       fonts: 'bootstrap/fonts'
+         },
+         fonts: 'bootstrap/fonts'
        },
        watch: {
          jss: {
