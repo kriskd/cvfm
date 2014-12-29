c83e11a50a456e535800c46be17d2de61e2acfd6
Kris Dockter <kriskd@gmail.com>
2014-12-29 13:32:56 -0600

    Content pages

--- Controller/PagesController.php
+++ Controller/PagesController.php
@@ -54,38 +54,31 @@ class PagesController extends AppController {
 	public function display() { 
 		$path = func_get_args();
 		$slug = current($path); 
-		
+
 		if ($content = $this->Page->find('first', array('conditions' => array('uri' => $slug)))) {
 			$this->set('content', $content);
-		}
-		
-		$events = $this->Event->find('all', array(
-							'conditions' => array(
-								'date >=' => date('Y-m-d', strtotime("+1 day"))
-							),
-							'fields' => array(
-								'date', 'description'
-							)
-						)
-					); 
+            $this->render('page', 'cvfm');
+		} else {
 
-		$count = count($path);
-		if (!$count) {
-			$this->redirect('/');
-		}
-		$page = $subpage = $title_for_layout = null;
+            $count = count($path);
+            if (!$count) {
+                $this->redirect('/');
+            }
+            $page = $subpage = $title_for_layout = null;
 
-		if (!empty($path[0])) {
-			$page = $path[0];
-		}
-		if (!empty($path[1])) {
-			$subpage = $path[1];
-		}
-		if (!empty($path[$count - 1])) {
-			$title_for_layout = Inflector::humanize($path[$count - 1]);
-		}
-		$this->set(compact('page', 'subpage', 'title_for_layout', 'events'));
-		$this->render(implode('/', $path), 'cvfm');
+            if (!empty($path[0])) {
+                $page = $path[0];
+            }
+            if (!empty($path[1])) {
+                $subpage = $path[1];
+            }
+            if (!empty($path[$count - 1])) {
+                $title_for_layout = Inflector::humanize($path[$count - 1]);
+            }
+            
+            $this->set(compact('page', 'subpage', 'title_for_layout'));
+            $this->render(implode('/', $path), 'cvfm');
+        }
 	}
 	
 	public function admin_index()
