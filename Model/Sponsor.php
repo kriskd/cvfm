3e4de3d5a56411929f5e315babf09410555697c5
Kris Dockter <kriskd@gmail.com>
2015-02-20 20:02:03 -0600

    Validation for sponsor type

--- Model/Sponsor.php
+++ Model/Sponsor.php
@@ -9,9 +9,14 @@ class Sponsor extends AppModel {
                 'name_not_empty' => array(
                     'rule' => 'notEmpty'
                     ,'message' => 'Sponsor name is required.')
+                ),
+            'type' => array(
+                'name_not_empty' => array(
+                    'rule' => 'notEmpty',
+                    'message' => 'Specify type of sponsor.'
                 )
-
-            ,'website' => array(
+            ),
+            'website' => array(
                 'valid_url' => array(
                     'rule' => array('url', true),
                     'allowEmpty' => true,
