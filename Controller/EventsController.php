f8775f7f6e8ba6e8a677112905f346dde215f394
Kris Dockter <kriskd@gmail.com>
2019-04-19 15:05:14 -0500

    File cleanup

--- Controller/EventsController.php
+++ Controller/EventsController.php
@@ -8,12 +8,12 @@ App::uses('AppController', 'Controller');
  */
 class EventsController extends AppController {
 
-/**
- * Components
- *
- * @var array
- */
-	public $components = array('Paginator');
+    /**
+     * Components
+     *
+     * @var array
+     */
+    public $components = array('Paginator');
     public $paginate = array(
         'order' => 'Event.date ASC',
         'limit' => 10,
@@ -25,62 +25,62 @@ class EventsController extends AppController {
     }
 
     public function index() {
-		$events = $this->Event->find('all', array(
-                'conditions' => array(
-                    'date >=' => date('Y-m-d')
-                ),
-                'fields' => array(
-                    'date', 'description'
-                ),
-                'order' => array(
-                    'date ASC',
-                ),
-            )
-        ); 
+        $events = $this->Event->find('all', array(
+            'conditions' => array(
+                'date >=' => date('Y-m-d')
+            ),
+            'fields' => array(
+                'date', 'description'
+            ),
+            'order' => array(
+                'date ASC',
+            ),
+        )
+    );
         $this->set(array('events' => $events));
     }
 
-/**
- * admin_index method
- *
- * @return void
- */
-	public function admin_index() {
-		$this->Event->recursive = 0;
+    /**
+     * admin_index method
+     *
+     * @return void
+     */
+    public function admin_index() {
+        $this->Event->recursive = 0;
         $this->Paginator->settings = $this->paginate;
-		$this->set('events', $this->Paginator->paginate());
-	}
+        $this->set('events', $this->Paginator->paginate());
+    }
 
-/**
- * admin_view method
- *
- * @throws NotFoundException
- * @param string $id
- * @return void
- */
-	public function admin_view($id = null) {
-		if (!$this->Event->exists($id)) {
-			throw new NotFoundException(__('Invalid event'));
-		}
-		$options = array('conditions' => array('Event.' . $this->Event->primaryKey => $id));
-		$this->set('event', $this->Event->find('first', $options));
-	}
+    /**
+     * admin_view method
+     *
+     * @throws NotFoundException
+     * @param string $id
+     * @return void
+     */
+    public function admin_view($id = null) {
+        if (!$this->Event->exists($id)) {
+            throw new NotFoundException(__('Invalid event'));
+        }
+        $options = array('conditions' => array('Event.' . $this->Event->primaryKey => $id));
+        $this->set('event', $this->Event->find('first', $options));
+    }
 
-/**
- * admin_add method
- *
- * @return void
- */
-	public function admin_add() {
-		if ($this->request->is('post')) {
-			$this->Event->create();
-			if ($this->Event->save($this->request->data, ['validate' => false])) {
-				$this->Flash->success(__('The event has been saved.'));
-				return $this->redirect(array('action' => 'index'));
-			} else {
-				$this->Flash->danger(__('The event could not be saved. Please, try again.'));
-			}
-		}
+    /**
+     * admin_add method
+     *
+     * @return void
+     */
+    public function admin_add() {
+        if ($this->request->is('post')) {
+            $this->Event->create();
+            if ($this->Event->save($this->request->data, ['validate' => false])) {
+                $this->Flash->success(__('The event has been saved.'));
+                return $this->redirect(array('action' => 'index'));
+            } else {
+                $this->Flash->danger(__('The event could not be saved. Please, try again.'));
+            }
+        }
         if ($this->request->is('ajax') && isset($this->request->query['data'])) {
             $this->Event->set($this->request->query['data']);
             if ($this->Event->validates()) {
@@ -90,53 +90,53 @@ class EventsController extends AppController {
             }
         }
         $this->layout = 'ajax';
-	}
+    }
 
-/**
- * admin_edit method
- *
- * @throws NotFoundException
- * @param string $id
- * @return void
- */
-	public function admin_edit($id = null) {
-		if (!$this->Event->exists($id)) {
-			throw new NotFoundException(__('Invalid event'));
-		}
-		if ($this->request->is(array('post', 'put'))) {
-			if ($this->Event->save($this->request->data)) {
-				$this->Flash->success(__('The event has been saved.'));
-				return $this->redirect(array('action' => 'index'));
-			} else {
-				$this->Flash->danger(__('The event could not be saved. Please, try again.'));
-			}
-		} 
+    /**
+     * admin_edit method
+     *
+     * @throws NotFoundException
+     * @param string $id
+     * @return void
+     */
+    public function admin_edit($id = null) {
+        if (!$this->Event->exists($id)) {
+            throw new NotFoundException(__('Invalid event'));
+        }
+        if ($this->request->is(array('post', 'put'))) {
+            if ($this->Event->save($this->request->data)) {
+                $this->Flash->success(__('The event has been saved.'));
+                return $this->redirect(array('action' => 'index'));
+            } else {
+                $this->Flash->danger(__('The event could not be saved. Please, try again.'));
+            }
+        } 
         $options = array('conditions' => array('Event.' . $this->Event->primaryKey => $id));
         $event = $this->Event->find('first', $options);
         $this->set('event', $event);
         $this->request->data = $event;
         $this->request->data['Event']['date_pick'] = date('m/d/Y', strtotime($event['Event']['date']));
-	}
-
-/**
- * admin_delete method
- *
- * @throws NotFoundException
- * @param string $id
- * @return void
- */
-	public function admin_delete($id = null) {
-		$this->Event->id = $id;
-		if (!$this->Event->exists()) {
-			throw new NotFoundException(__('Invalid event'));
-		}
-		$this->request->onlyAllow('post', 'delete');
-		if ($this->Event->delete()) {
-			$this->Flash->success(__('The event has been deleted.'));
-		} else {
-			$this->Flash->danger(__('The event could not be deleted. Please, try again.'));
-		}
-		return $this->redirect(array('action' => 'index'));
     }
 
+    /**
+     * admin_delete method
+     *
+     * @throws NotFoundException
+     * @param string $id
+     * @return void
+     */
+    public function admin_delete($id = null) {
+        $this->Event->id = $id;
+        if (!$this->Event->exists()) {
+            throw new NotFoundException(__('Invalid event'));
+        }
+        $this->request->onlyAllow('post', 'delete');
+        if ($this->Event->delete()) {
+            $this->Flash->success(__('The event has been deleted.'));
+        } else {
+            $this->Flash->danger(__('The event could not be deleted. Please, try again.'));
+        }
+        
+        return $this->redirect(array('action' => 'index'));
+    }
 }
