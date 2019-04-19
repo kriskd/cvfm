8433e27475ece266d085fdacf10f9cafe956e175
Kris Dockter <kriskd@gmail.com>
2019-04-19 15:05:06 -0500

    Update file for path to ShellDispatcher

--- Console/cake.php
+++ Console/cake.php
@@ -18,21 +18,29 @@
  * @since         CakePHP(tm) v 2.0
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
-$ds = DIRECTORY_SEPARATOR;
-$root = dirname(dirname(dirname(__FILE__)));
-$dispatcher = $root . $ds . 'localhost' . $ds . 'Lib' . $ds . 'Cake' . $ds . 'Console' . $ds . 'ShellDispatcher.php';
+if (!defined('DS')) {
+        define('DS', DIRECTORY_SEPARATOR);
+}
+
+$dispatcher = 'Cake' . DS . 'Console' . DS . 'ShellDispatcher.php';
 
 if (function_exists('ini_set')) {
-	$root = dirname(dirname(dirname(__FILE__)));
+    $root = dirname(dirname(dirname(__FILE__)));
+    $appDir = basename(dirname(dirname(__FILE__)));
+    $install = $root . DS . 'lib';
+    $composerInstall = $root . DS . $appDir . DS . 'Vendor' . DS . 'cakephp' . DS . 'cakephp' . DS . 'lib';
 
-	// the following line differs from its sibling
-	// /lib/Cake/Console/Templates/skel/Console/cake.php
-	ini_set('include_path', $root . $ds . 'lib' . PATH_SEPARATOR . ini_get('include_path'));
+    // the following lines differ from its sibling
+    // /lib/Cake/Console/Templates/skel/Console/cake.php
+    if (file_exists($composerInstall . DS . $dispatcher)) {
+        $install = $composerInstall;
+    }
+
+    ini_set('include_path', $install . PATH_SEPARATOR . ini_get('include_path'));
+    unset($root, $appDir, $install, $composerInstall);
 }
-
-if (!include($dispatcher)) {
-	trigger_error('Could not locate CakePHP core files.', E_USER_ERROR);
+if (!include $dispatcher) {
+    trigger_error('Could not locate CakePHP core files.', E_USER_ERROR);
 }
-unset($paths, $path, $dispatcher, $root, $ds);
-
+unset($dispatcher);
 return ShellDispatcher::run($argv);
